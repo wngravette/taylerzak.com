e1d925279f2eb4a7168b99fec3b5b4832001ac5d
William Naughton-Gravette <william@willng.me>
2015-10-02 19:11:53 +1000

    flog

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <h1>Tayler Zak Heaney</h1>
-<h3>If you are under the age of 18, click <a href="#">here</a>. If you're over 18, click <a href="#">here</a> and proceed with caution.</h3>
+<h3>If you are <span style="color:red">under the age of 18</span>, click <a href="#">here</a>. If you're over 18, click <a href="#">here</a> and proceed with caution.</h3>
