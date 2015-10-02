95385967123a4a60fab5ea33eb6f9dc4d9532482
William Naughton-Gravette <william@willng.me>
2015-10-02 19:09:47 +1000

    update splash

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-Tayler Zak Heaney
+<h1>Tayler Zak Heaney</h1>
+<h3>If you are under the age of 18, click <a href="#">here</a>. If you're over 18, click <a href="#">here</a> and proceed with caution.</h3>
